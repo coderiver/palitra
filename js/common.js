3727da92bc12e53b72d21232bfecf0e828c5aba7
akella <akella.a@gmail.com>
2014-07-07 15:38:52 +0300

    maps fix

--- js/common.js
+++ js/common.js
@@ -1,5 +1,13 @@
 head.ready(function() {
-
+	$('.shops__tomap').click(function(event) {
+		$('.shops__map').hide();
+		$('.bigmap').height($('.shops__map').height());
+		idd = $(this).attr('href');
+		$(idd+'').show();
+		$('html, body').animate({
+	        scrollTop: $(".bigmap").offset().top
+	    }, 500);
+	});
 	$('body').addClass('a');
 	$( ".maingl" ).on( "click", ".maingl__slitem", function() {
 		

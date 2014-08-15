970e250b302b8b0cd145c0053d4bd3400a610482
akella <akella.a@gmail.com>
2014-08-15 12:43:22 +0300

    fix logos in main

--- js/up.js
+++ js/up.js
@@ -3,4 +3,22 @@ head.ready(function() {
 $('#more-citiesup').click(function(event) {
 	$(".shops__cities").slideToggle("normal"); return false;
 });
+
+function checklogos(){
+	if($(window).width()>1390){
+		$('.map__logoswrap').cycle('pause');
+	}
+	else{
+		$('.map__logoswrap').cycle('play');
+	}
+}
+
+$(window).resize(function(event) {
+	checklogos();
+});
+
+setTimeout(function(){
+	checklogos();
+}, 300);
+
 });

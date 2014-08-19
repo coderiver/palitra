accde15e0fe184061fa1ede5306e568fff601947
akella <akella.a@gmail.com>
2014-08-20 01:24:45 +0300

    palitra logos slider

--- js/up.js
+++ js/up.js
@@ -4,21 +4,5 @@ $('#more-citiesup').click(function(event) {
 	$(".shops__cities").slideToggle("normal"); return false;
 });
 
-function checklogos(){
-	if($(window).width()>1390){
-		$('.map__logoswrap').cycle('pause');
-	}
-	else{
-		$('.map__logoswrap').cycle('play');
-	}
-}
-
-$(window).resize(function(event) {
-	checklogos();
-});
-
-setTimeout(function(){
-	checklogos();
-}, 300);
 
 });
